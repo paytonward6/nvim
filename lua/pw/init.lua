71dd36cbefa8071371f63de94b5b6507a6b0d017
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2023-11-16 18:24:09 -0600

    Move plugin setups to after/

--- lua/pw/init.lua
+++ lua/pw/init.lua
@@ -4,11 +4,3 @@ require("pw.abbrevs")
 local no_plugins = require("pw.plugins") -- plugins
 
 require("pw.snippets")
-require("pw.plugin-conf.treesitter")
-require("pw.plugin-conf.telescope")
-require("pw.plugin-conf.nvm-cmp")
-require("pw.plugin-conf.lualine")
-require("pw.plugin-conf.orgmode")
-require("pw.plugin-conf.whichkey")
-require("pw.plugin-conf.indent-blankline")
-require("pw.plugin-conf.mason")
