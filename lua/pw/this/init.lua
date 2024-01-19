cb8400bf45751adc2dfeae1ab594ad7bf218e41f
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2024-01-19 12:24:52 -0600

    Restore position of cursor after running Black

--- lua/pw/this/init.lua
+++ lua/pw/this/init.lua
@@ -64,7 +64,15 @@ M.dothat = function(opts)
 end
 
 M.black = function()
+    cursor_pos = vim.api.nvim_win_get_cursor(0)
     vim.api.nvim_command("%!black - -q")
+
+    line_count = vim.api.nvim_buf_line_count(0)
+    if cursor_pos[1] > line_count then
+        vim.api.nvim_win_set_cursor(0, {line_count, cursor_pos[2]})
+    else
+        vim.api.nvim_win_set_cursor(0, cursor_pos)
+    end
 end
 
 vim.api.nvim_create_user_command("Asdf", M.asdf, {nargs = 1})
