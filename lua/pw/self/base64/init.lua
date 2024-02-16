51a0b2f56b82a33256d4195918b18963a5e0a953
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2024-02-16 16:08:18 -0600

    Adds base64 command

---
+++ lua/pw/self/base64/init.lua
@@ -0,0 +1,29 @@
+M = {}
+
+
+function M.main(base64_arg)
+    local line = vim.api.nvim_get_current_line()
+    local split_line  = vim.fn.split(line, ":")
+    local key = split_line[1]
+    local value = vim.fn.trim(split_line[2])
+    local command = "echo ".. "'" .. value .. "'" .. " | base64"
+
+    if base64_arg ==  "d" then
+        command = command .. " -d"
+    end
+
+    local decoded = vim.fn.system(command)
+    local new_current_line = key .. ": " .. vim.fn.trim(decoded)
+    vim.api.nvim_set_current_line(new_current_line)
+end
+
+
+function M.base64(context)
+    M.main(context.args)
+end
+
+vim.api.nvim_create_user_command("Base64", M.base64, {
+    nargs = "?",
+    range = true,
+})
+
--- lua/pw/self/init.lua
+++ lua/pw/self/init.lua
@@ -1,3 +1,4 @@
 local path = "pw.self."
 require(path.."this")
 require(path.."black")
+require(path.."base64")
