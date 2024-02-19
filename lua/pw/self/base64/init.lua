acf6afd69824784b1512ea1156380a8e3be2ffe9
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2024-02-19 12:51:40 -0600

    Add Base64Encode and Base64Decode commands that takes yaml param

--- lua/pw/self/base64/init.lua
+++ lua/pw/self/base64/init.lua
@@ -14,39 +14,62 @@ local function split(line, delim)
     return {key, value}
 end
 
+function M.build_command(line, mode)
+    local command = "echo -n ".. vim.fn.shellescape(line) .. " | base64"
 
-function M.base64_line(line, base64_arg)
-    local split_line  = split(line, ":")
-    local key = split_line[1]
-    local value = vim.fn.trim(split_line[2])
-    local command = "echo -n ".. vim.fn.shellescape(value) .. " | base64"
-
-    if base64_arg ==  "d" then
+    if mode ==  "decode" then
         command = command .. " -d"
     end
-
-    local decoded = vim.fn.system(command)
-    return key .. ": " .. vim.fn.trim(decoded)
+    return command
 end
 
 
-function M.base64(start, last, base64_arg)
+function M.base64_line(line, format, mode)
+    if format == "yaml" then
+        local split_line  = split(line, ":")
+        local key = split_line[1]
+        local value = vim.fn.trim(split_line[2])
+
+        local command = M.build_command(value, mode)
+
+        local result = vim.fn.system(command)
+        return key .. ": " .. vim.fn.trim(result)
+    else
+        local command = M.build_command(vim.fn.trim(line), mode)
+        local result = vim.fn.system(command)
+        return vim.fn.trim(result)
+    end
+end
+
+
+function M.base64(start, last, args, mode)
     local lines = vim.api.nvim_buf_get_lines(0, start-1, last, false)
 
     local to_output = {}
     for _, line in ipairs(lines) do
-        table.insert(to_output, M.base64_line(line, base64_arg))
+        table.insert(to_output, M.base64_line(line, args, mode))
     end
     vim.api.nvim_buf_set_lines(0, start-1, last, false, to_output)
 end
 
-
-function M.main(context)
-    M.base64(context.line1, context.line2, context.args)
+function M.shared_entrypoint(context, mode)
+    M.base64(context.line1, context.line2, context.args, mode)
 end
 
-vim.api.nvim_create_user_command("Base64", M.main, {
+function M.decode(context)
+    M.shared_entrypoint(context, "decode")
+end
+
+function M.encode(context)
+    M.shared_entrypoint(context, nil)
+end
+
+vim.api.nvim_create_user_command("Base64Decode", M.decode, {
     nargs = "?",
     range = true,
 })
 
+vim.api.nvim_create_user_command("Base64Encode", M.encode, {
+    nargs = "?",
+    range = true,
+})
