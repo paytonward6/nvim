fc335cd32c51066fc0089e0399ea3c28efe2f210
paytonward6 <paytongward@gmail.com>
2024-12-12 08:39:44 -0600

    split plugins into separate files

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>gh', ':vertical Git<CR>', { desc = '[G]it open' })
-    vim.api.nvim_set_keymap('n', '<leader>gg', ':Git ', { desc = '[G]it commandline' })
+    vim.keymap.set('n', '<leader>gh', ':vertical Git<CR>', { desc = '[G]it open' })
+    vim.keymap.set('n', '<leader>gg', ':Git ', { desc = '[G]it commandline' })
   end,
 }
